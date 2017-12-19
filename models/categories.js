dbedd8e99ffd2804071eebf0008fbcfe6685bbc7
Tohir <thohiru.omoloye@talentbase.ng>
2017-12-19 06:25:01 +0000

    Visa categories model

---
+++ models/categories.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose')
+
+// VISA categories schema
+var categorySchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    create_date:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+var Category = module.exports = mongoose.model('Category', locationSchema)
+
+// get categories
+module.exports.getCategories = function(callback, limit){
+    Category.find(callback).limit(limit)
+}
+
+// add category 
+module.exports.addCategory = function(category, callback){
+    Category.create(category, callback)
+}
+
+// Update Category
+module.exports.updateCategory = function(id, category, options, callback){
+    var query = {_id: id}
+    var update = {
+        name: category.name
+    }
+    Genre.findOneAndUpdate(query, update, options, callback)
+}
+
+// remove category
+module.exports.removeCategory = function(id, callback){
+    var query = {_id: id}
+    Category.remove(query, callback)
+}
